9bb3ac33bb1c053f640e576db576bb7e052b2a2f
Robson Luis <robson.santos1313@yahoo.com>
2026-02-22 10:08:14 -0300

    Informatica

--- funcoes.js
+++ funcoes.js
@@ -1,1 +1,13 @@
 
+document.getElementById("meuForm").addEventListener("submit", function(event) {
+    
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let mensagem = document.getElementById("mensagem").value;
+
+    if (nome === "" || email === "" || mensagem === "") {
+        alert("Preencha todos os campos!");
+        event.preventDefault(); // impede envio
+    }
+
+});
